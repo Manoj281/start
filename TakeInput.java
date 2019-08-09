9d1ba2b6dc7af217949ff23503961da8140f6c8f
manoj <manoj@manojs-macbook.local>
2019-08-09 21:36:12 +0200

    ver 1.1

---
+++ TakeInput.java
@@ -0,0 +1,9 @@
+
+public class TakeInput extends HelloWorld{
+	public static void main(String args[]) {
+		HelloWorld Hi = new HelloWorld();
+		System.out.println(Hi.displayValue());
+		Hi.vmain();
+	}
+
+}
