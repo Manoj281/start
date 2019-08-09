315ff105baac613aa25eb0045e52e145332df7d3
manoj <manoj@manojs-macbook.local>
2019-08-09 21:20:39 +0200

    pushed

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,6 @@
-Class HelloWorld {
-    public static void main(String[] args) {
-    System.out.print("hello World");
+ 
+public class HelloWorld {
+    public static void main(String args[]) {
+       System.out.print("hello World");
     }
   }
